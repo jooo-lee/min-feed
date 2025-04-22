5ac59dfa0c29c5f88021c79f57c6dbfbaa92530c
Joseph Lee <josephlee3004@gmail.com>
2025-04-22 10:15:35 -0400

    Link to post rather than displaying content

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -15,25 +15,10 @@ const getFeeds = async (req, res, next) => {
 const getFeed = async (req, res, next) => {
   const feedId = req.params.feedId;
   const feed = feeds[feedId];
-  if (feed === 'undefined' || feed === null) {
+  if (typeof feed === 'undefined' || feed === null) {
     next(new Error('Could not get feed.'));
   }
   res.render('feed', { feed: feeds[feedId], feedId: feedId });
 };
 
-const getPost = async (req, res, next) => {
-  const feed = feeds[req.params.feedId];
-  const post = feed['items'][req.params.postId];
-  if (feed === 'undefined' || feed === null) {
-    next(new Error('Could not get feed.'));
-  } else if (post === 'undefined' || post === null) {
-    next(new Error('Could not get post.'));
-  }
-  try {
-    res.render('post', { post: post });
-  } catch (err) {
-    next(new Error('Could not get post.'));
-  }
-};
-
-export { getFeeds, getFeed, getPost };
+export { getFeeds, getFeed };
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -6,6 +6,5 @@ const router = Router();
 
 router.get('/', indexController.getFeeds);
 router.get('/feed/:feedId', indexController.getFeed);
-router.get('/feed/:feedId/:postId', indexController.getPost);
 
 export { router as indexRouter };
